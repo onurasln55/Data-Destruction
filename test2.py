64b2cda97ef47015e3f9b687515d26bf1e35ca9b
onurasln55 <onurasln55@gmail.com>
2019-12-06 17:11:47 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,21 +1,30 @@
 import tkinter
-
+window = tkinter.Tk()
 def page1():
     page2text.pack_forget()
+    page3text.pack_forget()
     page1text.pack()
 
 def page2():
     page1text.pack_forget()
     page2text.pack()
+    page3text.pack_forget()
 
-window = tkinter.Tk()
-
-page1btn = tkinter.Button(window, text="Page 1", command=page1)
-page2btn = tkinter.Button(window, text="Page 2", command=page2)
+def page3():
+    page1text.pack_forget()
+    page2text.pack_forget()
+    page3text.pack()
+pagelist="123"
+i=iter(pagelist)
+page="page"
+page1btn = tkinter.Button(window, text="Next", command=page+next(i))
+page2btn = tkinter.Button(window, text="Çık", command=quit)
 
 page1text = tkinter.Label(window, text="This is page 1")
 page2text = tkinter.Label(window, text="This is page 2")
+page3text = tkinter.Label(window, text="This is page 3")
 
 page1btn.pack()
 page2btn.pack()
 page1text.pack()
+window.mainloop()
