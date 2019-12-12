a65586442dde342df53300e785fb18f2477b2c35
onurasln55 <onurasln55@gmail.com>
2019-12-12 17:33:35 +0300

    Update 6.py

--- 6.py
+++ 6.py
@@ -97,7 +97,7 @@ def refresh():
             serial[x]=serial[x].replace('\n','')
             model[x]=model[x].replace('\n','')
             name[x]=name[x].replace('\n','')
-            mlb.insert(END, ('%d' % name[x], '%d'% model[x], '%d' % serial[x], '%d'% size[x], pgbars["1"].pack(),'%d'% state[x],))
+            mlb.insert(END, ('%s' % name[x], '%s'% model[x], '%s' % serial[x], '%s'% size[x], pgbars["1"].pack(),'%s'% state[x],))
 
 
 
